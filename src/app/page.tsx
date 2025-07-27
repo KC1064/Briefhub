9e220f2c920d0cb7067b40c732d518d324cb47b2
KC1064 <kiron08072003@gmail.com>
2025-07-27 17:56:27 +0530

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+
+export default async function Home() {
+  return <div className="text-red-500">
+   <Button>Text Here</Button>
+  </div>
+}
